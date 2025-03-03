840d1bcf3f71cba9a712d07580b75b6d6f97f0f2
Niloydey59 <132149288+Niloydey59@users.noreply.github.com>
2025-03-03 10:16:45 +0600

    Update base.js

--- client_react/src/FetchApi/base.js
+++ client_react/src/FetchApi/base.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://stack-ruet-backend.onrender.com"; // Backend URL
+const API_URL = "https://stack-ruet-backend.onrender.com/api"; // Backend URL
 
 const api = axios.create({
   baseURL: API_URL,
